450f8f67c1aabc143687d0c2884ae5d2620133e2
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-08 14:10:17 +0300

    My very fourteenth GO commit

---
+++ strlen.go
@@ -0,0 +1,15 @@
+//Write a function that counts the runes of a string and that returns that count.
+
+package main
+
+func StrLen(s string) int{
+
+	value := "Hello World!"
+
+    length := len(value)
+    	Println(length)
+
+    for i := 0; i < len(value); i++ {
+        Println(string(value[i]))
+}
+
