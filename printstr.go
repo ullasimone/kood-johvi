114eaf6e11e53f7535a90025eb3a772995f78d4c
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-08 13:39:37 +0300

    My very thirteenth GO commit

--- printstr.go
+++ printstr.go
@@ -3,19 +3,8 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	var aRune string = "Hello World!"
-
-	z01.PrintRune(rune(aRune[0]))
-	z01.PrintRune(rune(aRune[1]))
-	z01.PrintRune(rune(aRune[2]))
-	z01.PrintRune(rune(aRune[3]))
-	z01.PrintRune(rune(aRune[4]))
-	z01.PrintRune(rune(aRune[5]))
-	z01.PrintRune(rune(aRune[6]))
-	z01.PrintRune(rune(aRune[7]))
-	z01.PrintRune(rune(aRune[8]))
-	z01.PrintRune(rune(aRune[9]))
-	z01.PrintRune(rune(aRune[10]))
-	z01.PrintRune(rune(aRune[11]))
-	z01.PrintRune('\n')
+	str := string
+	for _, s := range str {
+		z01.PrintRune(s)
+	}
 }
