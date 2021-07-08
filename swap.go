64cb70a42681c1b6381c520286c1e859a408841a
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-08 14:29:18 +0300

    My very quinze GO commit

---
+++ swap.go
@@ -0,0 +1,11 @@
+//Write a function that takes two pointers to an int (*int) and swaps their contents.
+
+package piscine
+
+func Swap(a *int, b *int){
+
+	var c *int := *c
+	*a = *c
+	*b = *a
+	*c = *b
+}
