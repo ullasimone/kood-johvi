1ee710dde1c0653f6a2c7588720b4746add81412
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-12 11:40:34 +0300

    Quest04 first commit

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,6 +1,3 @@
-/*Write an iterative function that returns the factorial of the int passed as parameter.
-Errors (non possible values or overflows) will return 0.*/
-
 package piscine
 
 func IterativeFactorial(nb int) int {
@@ -14,8 +11,3 @@ func IterativeFactorial(nb int) int {
 		return result
 	}
 }
-
-/*func main() {
-	arg := 4
-	fmt.Println(IterativeFactorial(arg))
-}*/
