c17fbf417bbc454a2368420b03ccc496b9f7c21a
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-12 23:36:04 +0300

    Quest04 FindNextPrime solution

--- findnextprime.go
+++ findnextprime.go
@@ -4,36 +4,16 @@ The function must be optimized in order to avoid time-outs with the tester.
 
 package piscine
 
-/*import (
-	"fmt"
-)*/
-
-func kapsas(nb int) bool {
-	// optimize below 2
-	if nb < 2 {
-		return false
-	} // loop - start to search
-	for i := 2; i < nb; i++ {
-		// divide with i, if it has decimal, not a prime number
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func FindNextPrime(nb int) int {
-	if nb <= 2 {
-		return nb
-	}
-	for i := nb; ; i++ {
-		if kapsas(i) {
-			return i
+	primeNb := true
+
+	for IsPrime(nb) {
+		if IsPrime(nb) == primeNb {
+			return nb
+		}
+		for IsPrime(nb) != primeNb {
+			return nb
 		}
 	}
+	return 0
 }
-
-/*func main() {
-	fmt.Println(FindNextPrime(16))
-	fmt.Println(FindNextPrime(7))
-}*/
