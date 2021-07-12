8ba9f852b3cb1ea822ef20ec291ed8ef40419250
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-12 16:05:33 +0300

    Quest04 fifth commit

--- fibonacci.go
+++ fibonacci.go
@@ -3,11 +3,17 @@ package piscine
 // import "fmt"
 
 func Fibonacci(index int) int {
-	if index <= 1 {
-		return index
-	} else {
-		return Fibonacci(index-1) + Fibonacci(index-2)
+	if index < 0 {
+		return -1
 	}
+	if index == 0 {
+		return 0
+	}
+	if index == 1 {
+		return 1
+	}
+
+	return Fibonacci(index-1) + Fibonacci(index-2)
 }
 
 /*func main() {
