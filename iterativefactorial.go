5f5344a0b94dbe4a191fcf31e5ee2d35bae3c4e6
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-12 12:00:50 +0300

    Quest04 first commit

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,23 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 {
-		return 0
-	} else {
-		result := 1
+	result := 1
+	// factorial number
+	if nb == 1 || nb == 0 {
+		return 1
+		// i = 1, 'cause you can't get a valid number if you multiply with 0
+	} else if nb > 1 && nb <= 20 {
 		for i := 1; i < nb+1; i++ {
-			result = nb * i
+			result = result * i
 		}
 		return result
+		// everything under 0
+	} else {
+		return 0
 	}
 }
+
+/*func main() {
+	result1 := IterativeFactorial(4)
+	fmt.Println(result1)
+}*/
