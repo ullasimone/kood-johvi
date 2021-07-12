ef77d16979992df21cde089662b077099901eb13
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-12 14:30:01 +0300

    Quest04 fourth commit

--- iterativepower.go
+++ iterativepower.go
@@ -4,6 +4,8 @@ func IterativePower(nb int, power int) int {
 	result := nb
 	if power < 0 {
 		result = 0
+	} else if power == 0 {
+		result = 1
 	} else {
 		for i := 1; i < power; i++ {
 			result = result * nb
