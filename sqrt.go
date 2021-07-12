7f29e5231f068c443f8ac2273d6d06fe2d7ab6aa
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-12 17:37:16 +0300

    Quest04 sixth commit

---
+++ sqrt.go
@@ -0,0 +1,16 @@
+// Write a function that returns the square root of the int passed as parameter, if that square root is a whole number. Otherwise it returns 0.
+
+package piscine
+
+func Sqrt(nb int) int {
+	for i := 1; i <= nb; i++ {
+		if i*i == nb {
+			return i
+	}
+	return 0
+}
+
+/*func main() {
+	fmt.Println(Sqrt(4))
+	fmt.Println(Sqrt(3))
+}*/
