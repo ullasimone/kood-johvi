b97e127f7db070f2df6b0e4abf00c735f22dc10e
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-12 14:59:42 +0300

    Quest04 fourth commit

--- recursivepower.go
+++ recursivepower.go
@@ -1,12 +1,15 @@
 package piscine
 
+// import "fmt"
+
 func RecursivePower(nb int, power int) int {
-	if power > 0 {
-		return nb * RecursivePower(nb, power-1)
-	} else {
+	if power < 0 {
+		return 0
+	} else if power == 0 {
 		return 1
+	} else {
+		return nb * RecursivePower(nb, power-1)
 	}
-	return 1
 }
 
 /*func main() {
