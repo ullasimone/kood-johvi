7f24d63af64e545f6ab0e25062573e7a5b394eb4
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-13 11:12:33 +0300

    Quest04 FindNextPrime solution

--- findnextprime.go
+++ findnextprime.go
@@ -1,11 +1,7 @@
-/*Write a function that returns the first prime number that is equal or superior to the int passed as parameter.
-The function must be optimized in order to avoid time-outs with the tester.
-(We consider that only positive numbers can be prime numbers)*/
-
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb >= 2 {
+	if nb <= 2 {
 		return 2
 	}
 	for i := nb; ; i++ {
