0ae720e401d7a47b7a9702333c87763a38795b6a
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-13 11:53:11 +0300

    Quest05 NRune solution

--- nrune.go
+++ nrune.go
@@ -3,15 +3,14 @@
 
 package piscine
 
-
-func NRune(s string n int) rune {
+func NRune(s string, n int) rune {
 	runes := []rune(s)
 	len := 0
-	for i := range runes{
+	for i := range runes {
 		len = i
 	}
 
-	if n-1 >= 0 && n-1 <= len{
+	if n-1 >= 0 && n-1 <= len {
 		return runes[n-1]
 	}
 	return 0
