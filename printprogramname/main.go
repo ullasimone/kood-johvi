c45b30c3ff54148269d646beeafc3098a5662ce9
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-14 10:19:17 +0300

    Quest06 PrintProgramName solution

---
+++ printprogramname/main.go
@@ -0,0 +1,17 @@
+// Write a program that prints the name of the program.
+
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args
+	for _, i := range arg[0] {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
+}
