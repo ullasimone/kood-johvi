8f6c48b3821507f051b799e92372c67be2aaf67d
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-14 16:10:48 +0300

    Quest06 RevParams solution

---
+++ revparams/main.go
@@ -0,0 +1,23 @@
+// Write a program that prints the arguments received in the command line in reverse order.
+
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args
+	counter := 0
+	for s := range arg {
+		counter = s + 1
+	}
+	for j := counter - 1; j >= 1; j-- {
+		for _, l := range arg[j] {
+			z01.PrintRune(l)
+		}
+		z01.PrintRune(10)
+	}
+}
