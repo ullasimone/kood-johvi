aafe7e500f5ad78531323ff04674fe752fb4fd2f
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-14 12:06:27 +0300

    Quest06 PrintProgramName solution

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	arg := os.Args
-	for _, res := range arg[0] {
-		z01.PrintRune(res)
+	arg := []rune(os.Args[0])  // casting - type conversion; type translation; translate the string into an array of runes
+	for _, i := range arg[0] { // no need of index, (skip it (for _,)) then translate the rune array into symbols
+		z01.PrintRune(i)
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
