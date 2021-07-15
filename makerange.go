baea0e0f11ff420633dbbfb1c1ad0fab0b49495b
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-15 11:01:32 +0300

    Quest07 MakeRange solution

---
+++ makerange.go
@@ -0,0 +1,18 @@
+/*Write a function that takes an int min and an int max as parameters. That function returns a slice of int with all the values between min and max.
+Min is included, and max is excluded.
+If min is superior or equal to max, a nil slice is returned.
+append is not allowed for this exercise.*/
+
+package piscine
+
+func MakeRange(min, max int) []int {
+	size := max - min
+	answer := make([]int, size)
+	for i := 0; i < size; i++ {
+		answer[i] = min + i
+	}
+	if min >= max {
+		return nil
+	}
+	return answer
+}
