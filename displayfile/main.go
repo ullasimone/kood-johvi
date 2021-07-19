b34730f431b5d3e040b11ad90b830fec7235f7e6
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-19 14:35:23 +0300

    Quest08 DisplayFile solution

--- displayfile/main.go
+++ displayfile/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	file := "quest8.txt"
+	fileName := "quest8.txt"
 
 	if len(os.Args) < 2 {
 		fmt.Println("File name missing")
@@ -20,7 +20,7 @@ func main() {
 		return
 	}
 
-	data, err := ioutil.ReadFile(file)
+	data, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
