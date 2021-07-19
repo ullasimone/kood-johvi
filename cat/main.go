88ef36649967fcbbfb343070cbf0d5895656733b
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-19 14:59:06 +0300

    Quest08 Cat solution

---
+++ cat/main.go
@@ -0,0 +1,34 @@
+/*Write a program that behaves like a simplified cat command.
+The options do not have to be handled.
+If the program is called without arguments it should take the standard input (stdin) and print it back on the standard output (stdout).*/
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if 0 == len(args) {
+		fmt.Print()
+	} else {
+		for _, s := range os.Args[1:] {
+			file, err := os.Open(s)
+			if err != nil {
+				fmt.Println(err.Error())
+				break
+			} else {
+				data, err := ioutil.ReadAll(file)
+				if err != nil {
+					fmt.Println(err.Error())
+					break
+				} else {
+					fmt.Printf("%s", data)
+				}
+			}
+		}
+	}
+}
