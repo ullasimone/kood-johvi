45bba52026466a621dbb5980f6213b1be6bb385b
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-20 22:14:50 +0300

    Quest09 Doop solution

--- doop/main.go
+++ doop/main.go
@@ -8,57 +8,95 @@ The program has to handle the modulo and division operations by 0 as shown on th
 
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+import "os"
 
-func isOperator(s string, a []string) bool {
-	for _, val := range a {
-		if s == val {
-			return true
+func atoi(s string) (int, bool) {
+	sum := 0
+	minus := false
+	for i, c := range s {
+		if c == '-' && i == 0 {
+			minus = true
+		} else if c >= 48 && c <= 57 {
+			l := toInt(c)
+			sum = sum*10 + l
+		} else {
+			return 0, false
 		}
 	}
-	return false
+	if minus {
+		sum = -sum
+	}
+	return sum, true
+}
+
+func toInt(a rune) int {
+	s := 0
+	for i := '0'; i < a; i++ {
+		s++
+	}
+	return s
+}
+
+func itoa(n int) (s string) {
+	for i := n; i != 0; i = i / 10 {
+		zz := i % 10
+		if zz < 0 {
+			zz = -zz
+		}
+		s = string(rune(zz+48)) + s
+	}
+	if n < 0 {
+		s = "-" + s
+	}
+	if n == 0 {
+		s = "0"
+	}
+	return
 }
 
 func main() {
-	op := []string{"+", "*", "-", "/", "%"}
-	args := os.Args[1:]
-	if len(args) != 3 {
-	} else {
-		if isOperator(args[1], op) {
-			n1, err1 := strconv.Atoi(args[0])
-			n2, err2 := strconv.Atoi(args[2])
-			if err1 == nil && err2 == nil {
-				switch args[1] {
-				case "+":
-					fmt.Println(n1 + n2)
-				case "-":
-					fmt.Println(n1 - n2)
-				case "*":
-					fmt.Println(n1 * n2)
-				case "/":
-					if n2 == 0 {
-						fmt.Println("Error: no division by 0")
-					} else {
-						fmt.Println(n1 / n2)
-					}
-				case "%":
-					if n2 == 0 {
-						fmt.Println("Error: no modulo by 0")
-					} else {
-						fmt.Println(n1 % n2)
-					}
-
-				}
-			} else {
-				fmt.Println("1")
-			}
-
+	l := 0
+	for range os.Args {
+		l++
+	}
+	if l != 4 {
+		return
+	}
+	a, ok := atoi(os.Args[1])
+	if !ok {
+		return
+	}
+	b, ok := atoi(os.Args[3])
+	if !ok {
+		return
+	}
+	switch os.Args[2] {
+	case "+":
+		result := a + b
+		if (result > a) == (b > 0) {
+			os.Stdout.WriteString(itoa(result) + "\n")
+		}
+	case "-":
+		result := a - b
+		if (result < a) == (b > 0) {
+			os.Stdout.WriteString(itoa(result) + "\n")
+		}
+	case "*":
+		result := a * b
+		if a == 0 || (result/a == b) {
+			os.Stdout.WriteString(itoa(result) + "\n")
+		}
+	case "/":
+		if b == 0 {
+			os.Stdout.WriteString("No division by 0\n")
 		} else {
-			fmt.Println("0")
+			os.Stdout.WriteString(itoa(a/b) + "\n")
+		}
+	case "%":
+		if b == 0 {
+			os.Stdout.WriteString("No modulo by 0\n")
+		} else {
+			os.Stdout.WriteString(itoa(a%b) + "\n")
 		}
 	}
 }
