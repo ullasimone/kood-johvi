4a4c2bfc3eff298f5bb824f88bcfe8b133b44eff
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Air.local>
2021-07-20 23:53:58 +0300

    Quest08 Cat solution

--- cat/main.go
+++ cat/main.go
@@ -5,30 +5,39 @@ If the program is called without arguments it should take the standard input (st
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		return
-	} else {
-		os.Args = os.Args[1:]
-
-		for _, v := range os.Args {
-			file, err := os.Open(v)
-			if err != nil {
-				fmt.Println(err)
-				return
-			} else {
-				data := make([]byte, 443)
-				file.Read(data)
-				if len(os.Args) == 1 {
-					fmt.Println(string(data))
-				} else {
-					fmt.Println(string(data))
-				}
-				file.Close()
+	lens := 0
+	argum := os.Args[1:]
+	for range argum {
+		lens++
+	}
+	for i := 1; i <= lens; i++ {
+		file, err := ioutil.ReadFile(os.Args[i])
+		strFile := string(file)
+		if err != nil {
+			string := "open " + os.Args[i] + ": no such file or directory"
+			for _, v := range string {
+				z01.PrintRune(v)
+			}
+			z01.PrintRune('\n')
+		} else {
+			for _, v := range strFile {
+				z01.PrintRune(v)
+			}
+			z01.PrintRune('\n')
+		}
+	}
+	if lens == 0 {
+		for {
+			text, _ := ioutil.ReadAll(os.Stdin)
+			for _, v := range string(text) {
+				z01.PrintRune(v)
 			}
 		}
 	}
